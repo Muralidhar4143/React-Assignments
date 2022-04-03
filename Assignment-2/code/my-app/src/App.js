e088909f5b8673390110442ce0ffe214f1ea28fa
Muralidhar4143 <ragitimuralidhar@gmail.com>
2022-04-03 11:33:33 +0530

    React2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
-
-
-function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
+function App({employee}) {
+return(<div className="container">
+  <img className='employee_Img' src={employee.profileImg} alt="Employee_image"/>
+  <div className='employee_Name'>{employee.name}</div>
+  <EmployeeDetails  detail_Name={"Location"} details={employee.location}/>
+  <EmployeeDetails  detail_Name={"Blood Group"} details={employee.bloodGroup}/>
+  <EmployeeDetails  detail_Name={"Age"} details={employee.age}/>
 </div>
 )
 }
-
-
-
+function EmployeeDetails({detail_Name, details}){
+  return(
+  <div className='employeeDet_Wrapper'>
+    <div className='employee_Detail_Label'>{detail_Name}</div>
+    <div className='employee_Detail'>{details}</div>
+  </div>
+  )
+}
 export default App;

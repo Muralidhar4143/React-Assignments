1dcc595e4463c7703c685d972fd4aa2b83e2e838
Muralidhar4143 <ragitimuralidhar@gmail.com>
2022-04-13 11:36:35 +0530

    4

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Postview.css';
-
+import Allcomponent from './components/All_component'
 
 const Postview=()=> {
   return (
     <div className="site-container">
+      <Allcomponent/>
     </div>
   );
 }
-
 export default Postview;
